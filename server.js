398b6615c6327d9b431a6a32b7f779b0e5d23670
ThatBadName <thatbadname@outlook.com>
2025-05-30 16:55:39 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const { Bridge } = require("discord-cross-hosting");
+require("dotenv").config();
+
+const server = new Bridge({
+  port: parseInt(process.env["PORT"]),
+  authToken: "token",
+  totalShards: "auto", // The Total Shards of the Bot or 'auto'
+  totalMachines: 1, // The Total Machines, where the Clusters will run
+  shardsPerCluster: 16, // The amount of Internal Shards, which are in one Cluster
+  token: process.env["DISCORD_TOKEN"],
+});
+
+server.on("debug", console.log);
+server.start();
+server.on("ready", (url) => {
+  console.log("Server is ready" + url);
+});
